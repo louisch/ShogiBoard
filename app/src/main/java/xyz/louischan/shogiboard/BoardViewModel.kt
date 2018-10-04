134e8d2690ce3ef36a4503b4bbcd73f59f67e74c
Louis Chan <louisch@fastmail.com>
2018-10-04 23:57:07 +0100

    Render standard board

--- app/src/main/java/xyz/louischan/shogiboard/BoardViewModel.kt
+++ app/src/main/java/xyz/louischan/shogiboard/BoardViewModel.kt
@@ -1,8 +1,76 @@
 package xyz.louischan.shogiboard
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModel
 
-class BoardViewModel {
 
+class BoardViewModel : ViewModel() {
+
+    val board: ShogiBoard = makeStandardBoard()
+    val allChangeObservers = mutableListOf<Observer<ShogiBoard>>()
+    val moveObservers = mutableListOf<(Piece) -> Unit>()
+
+    fun observeAnyChange(observer: Observer<ShogiBoard>) {
+        allChangeObservers.add(observer)
+    }
+
+    fun observeMoves(observer: (Piece) -> Unit) {
+        moveObservers.add(observer)
+    }
+
+    fun movePiece(from: ShogiCoordinate, to: ShogiCoordinate) {
+        val piece = board.move(from, to)
+        if (piece != null) {
+            moveObservers.forEach { it(piece) }
+        }
+    }
+
+    fun changeBoard(func: (ShogiBoard) -> Unit) {
+        func(board)
+        allChangeObservers.forEach { it.onChanged(board) }
+    }
+}
+
+fun makeStandardBoard(): ShogiBoard {
+    val board = ShogiBoard()
+    val white = WHITEPLAYER()
+    val black = BLACKPLAYER()
+
+    board.setPiece(Piece.L(), white, ShogiCoordinate(1, 1))
+    board.setPiece(Piece.N(), white, ShogiCoordinate(2, 1))
+    board.setPiece(Piece.S(), white, ShogiCoordinate(3, 1))
+    board.setPiece(Piece.G(), white, ShogiCoordinate(4, 1))
+    board.setPiece(Piece.KB(), white, ShogiCoordinate(5, 1))
+    board.setPiece(Piece.G(), white, ShogiCoordinate(6, 1))
+    board.setPiece(Piece.S(), white, ShogiCoordinate(7, 1))
+    board.setPiece(Piece.N(), white, ShogiCoordinate(8, 1))
+    board.setPiece(Piece.L(), white, ShogiCoordinate(9, 1))
+
+    board.setPiece(Piece.R(), white, ShogiCoordinate(8, 2))
+    board.setPiece(Piece.B(), white, ShogiCoordinate(2, 2))
+
+    for (i in 1.until(10)) {
+        board.setPiece(Piece.P(), white, ShogiCoordinate(i, 3))
+    }
+
+    for (i in 1.until(10)) {
+        board.setPiece(Piece.P(), black, ShogiCoordinate(i, 7))
+    }
+
+    board.setPiece(Piece.R(), black, ShogiCoordinate(2, 8))
+    board.setPiece(Piece.B(), black, ShogiCoordinate(8, 8))
+
+    board.setPiece(Piece.L(), black, ShogiCoordinate(1, 9))
+    board.setPiece(Piece.N(), black, ShogiCoordinate(2, 9))
+    board.setPiece(Piece.S(), black, ShogiCoordinate(3, 9))
+    board.setPiece(Piece.G(), black, ShogiCoordinate(4, 9))
+    board.setPiece(Piece.KW(), black, ShogiCoordinate(5, 9))
+    board.setPiece(Piece.G(), black, ShogiCoordinate(6, 9))
+    board.setPiece(Piece.S(), black, ShogiCoordinate(7, 9))
+    board.setPiece(Piece.N(), black, ShogiCoordinate(8, 9))
+    board.setPiece(Piece.L(), black, ShogiCoordinate(9, 9))
+
+    return board
 }
 
 class ShogiBoard {
@@ -18,12 +86,13 @@ class ShogiBoard {
         return true
     }
 
-    fun move(from: ShogiCoordinate, to: ShogiCoordinate): Boolean {
-        if (isEmpty(to) || !isEmpty(to)) return false
+    fun move(from: ShogiCoordinate, to: ShogiCoordinate): Piece? {
+        if (isEmpty(to) || !isEmpty(to)) return null
 
-        setPiece(getPiece(from), to)
+        val piece = getPiece(from)
+        setPiece(piece, to)
         setPiece(null, from)
-        return true
+        return piece
     }
 
     fun isEmpty(coord: ShogiCoordinate): Boolean {
@@ -34,9 +103,16 @@ class ShogiBoard {
         return board[coords.getIndFile()][coords.getIndRank()]
     }
 
-    private fun setPiece(piece: Piece?, coords: ShogiCoordinate) {
+    fun setPiece(piece: Piece?, coords: ShogiCoordinate) {
         board[coords.getIndFile()][coords.getIndRank()] = piece
+    }
 
+    fun setPiece(type: Int, owner: Player, coords: ShogiCoordinate) {
+        setPiece(Piece(type, owner, coords), coords)
+    }
+
+    fun allPieces(): Iterable<Piece> {
+        return board.flatten().filterNotNull()
     }
 }
 
@@ -49,11 +125,41 @@ class ShogiCoordinate(val file: Int, val rank: Int) {
     fun getIndRank(): Int {
         return rank - 1
     }
+
+    override fun hashCode(): Int {
+        return file * 10 + rank
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShogiCoordinate
+
+        if (file != other.file) return false
+        if (rank != other.rank) return false
+
+        return true
+    }
 }
 
+class Piece(val type: Int, val owner: Player, val coords: ShogiCoordinate) {
 
-class Piece(val owner: Player, val drawableId: Int) {
+    fun identifier(): String {
+        return type.toString() + if (owner == BLACKPLAYER()) ":BLACK" else ":WHITE"
+    }
 
+    companion object Type {
+        fun P(): Int { return R.string.piece_name_P }
+        fun L(): Int { return R.string.piece_name_L }
+        fun N(): Int { return R.string.piece_name_N }
+        fun S(): Int { return R.string.piece_name_S }
+        fun G(): Int { return R.string.piece_name_G }
+        fun B(): Int { return R.string.piece_name_B }
+        fun R(): Int { return R.string.piece_name_R }
+        fun KB(): Int { return R.string.piece_name_KB }
+        fun KW(): Int { return R.string.piece_name_KW }
+    }
 }
 
 class Player(val id: Int) {
--- app/src/main/java/xyz/louischan/shogiboard/MainActivity.kt
+++ app/src/main/java/xyz/louischan/shogiboard/MainActivity.kt
@@ -1,15 +1,21 @@
 package xyz.louischan.shogiboard
 
 import android.animation.ObjectAnimator
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
+import xyz.louischan.shogiboard.R.layout.piece
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var testPiece: RelativeLayout
+    lateinit var model: BoardViewModel
+    val pieceMap = hashMapOf<String, PieceView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,32 +23,25 @@ class MainActivity : AppCompatActivity() {
 
         val layout = findViewById<RelativeLayout>(R.id.main_layout)
 
-        testPiece = createPieceAt(ShogiCoordinate(9, 1))
-        layout.addView(testPiece)
-        layout.addView(createPieceAt(ShogiCoordinate(1, 1)))
-        layout.addView(createPieceAt(ShogiCoordinate(9, 9)))
-        layout.addView(createPieceAt(ShogiCoordinate(1, 9)))
+        model = ViewModelProviders.of(this).get(BoardViewModel::class.java)
+        renderPieces(model.board.allPieces(), layout)
+
+        model.observeMoves { piece: Piece ->
+            movePiece(piece)
+        }
     }
 
-    fun createPieceAt(coords: ShogiCoordinate): RelativeLayout {
-        return pieceTo(this.layoutInflater.inflate(R.layout.piece, null) as RelativeLayout, coords)
+    private fun renderPieces(pieces: Iterable<Piece>, layout: RelativeLayout) {
+        for (piece in pieces) {
+            layout.addView(PieceView(piece).view)
+        }
     }
 
-    fun pieceTo(piece: RelativeLayout, coords: ShogiCoordinate): RelativeLayout {
-        val dimens = CoordsAsDimens(coords)
-
-        val layoutParams =
-                FrameLayout.LayoutParams(
-                        resources.getDimension(R.dimen.piece_width).toInt(),
-                        resources.getDimension(R.dimen.piece_height).toInt()).also {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                        it.marginStart = dimens.leftMargin
-                    }
-                    it.leftMargin = dimens.leftMargin
-                    it.topMargin = dimens.topMargin
-                }
-        piece.layoutParams = layoutParams
-        return piece
+    private fun movePiece(piece: Piece) {
+        val pieceView = pieceMap[piece.identifier()]
+        if (pieceView != null) {
+            movePieceTo(pieceView.view, piece.coords)
+        }
     }
 
     fun movePieceTo(piece: RelativeLayout, coords: ShogiCoordinate) {
@@ -59,6 +58,36 @@ class MainActivity : AppCompatActivity() {
                 duration = 2000
                 start()
             }
+        }
+    }
+
+    inner class PieceView(piece: Piece) {
+
+        val view: RelativeLayout = layoutInflater.inflate(R.layout.piece, null) as RelativeLayout
+        val identifier: String = piece.identifier()
+
+        init {
+            view.findViewById<TextView>(R.id.piece_text).text = getString(piece.type)
+
+            val dimens = CoordsAsDimens(piece.coords)
+
+            val layoutParams =
+                    FrameLayout.LayoutParams(
+                            resources.getDimension(R.dimen.piece_width).toInt(),
+                            resources.getDimension(R.dimen.piece_height).toInt()).also {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                            it.marginStart = dimens.leftMargin
+                        }
+                        it.leftMargin = dimens.leftMargin
+                        it.topMargin = dimens.topMargin
+                    }
+            view.layoutParams = layoutParams
+
+            if (piece.owner == WHITEPLAYER()) {
+                view.rotation = 180.0f
+            }
+
+            pieceMap[identifier] = this
         }
     }
 
